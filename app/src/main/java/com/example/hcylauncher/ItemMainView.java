eadb276b1e03e121e946019081c4556b77d073cf
hcy_win <duanxuesong12@126.com>
2023-03-10 18:47:50 +0800

    完成部分组件准备

---
+++ app/src/main/java/com/example/hcylauncher/ItemMainView.java
@@ -0,0 +1,48 @@
+package com.example.hcylauncher;
+
+import android.content.Context;
+import android.graphics.Rect;
+import android.util.AttributeSet;
+import android.view.LayoutInflater;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+
+import androidx.annotation.Nullable;
+import androidx.core.view.ViewCompat;
+
+public class ItemMainView extends RelativeLayout {
+    private ImageView icon;
+    private TextView txName;
+    public ItemMainView(Context context) {
+        this(context,null);
+    }
+
+    public ItemMainView(Context context, AttributeSet attrs) {
+        this(context, attrs,0);
+    }
+
+    public ItemMainView(Context context, AttributeSet attrs, int defStyleAttr) {
+        this(context, attrs, defStyleAttr,0);
+    }
+
+    public ItemMainView(Context context, AttributeSet attrs, int defStyleAttr, int defStyleRes) {
+        super(context, attrs, defStyleAttr, defStyleRes);
+        LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        inflater.inflate(R.layout.item_main_view, this, true);
+        icon=findViewById(R.id.img_icon);
+        txName=findViewById(R.id.tx_appname);
+        txName.setText("dahfhfiahg;hg;agh;afghafgihafgihfdgthtsrjdyjyjfaghiaf");
+    }
+
+    @Override
+    protected void onFocusChanged(boolean gainFocus, int direction, @Nullable Rect previouslyFocusedRect) {
+        super.onFocusChanged(gainFocus, direction, previouslyFocusedRect);
+        if(gainFocus){
+            ViewCompat.animate(this).scaleX(1.10f).scaleY(1.10f).translationZ(1).start();
+        }else {
+            ViewCompat.animate(this).scaleX(1.0f).scaleY(1.0f).translationZ(1).start();
+        }
+        txName.setSelected(gainFocus);
+    }
+}
